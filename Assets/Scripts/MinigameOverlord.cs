14a2af6e71319b322db8dde95af57c01aff083a0
Rodrigo <rodri5202@hotmail.com>
2025-07-26 17:39:40 -0300

    Minijuego skibidi
    la dificultad la gestiona un integer en skillStatic. Al iniciar la escena el game manager lo lee y spawnea skibidis dependiendo del valor
    0 = 1 skibidi
    1 = 2 skibidis
    2 = 4 skibidis
    
    la cantidad de veces que tenes que meter la sopapa es un random de 2 a 4.

---
+++ Assets/Scripts/MinigameOverlord.cs
@@ -0,0 +1,69 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MinigameOverlord : MonoBehaviour
+{
+    
+    //public MouseMovement playerPlunge;
+    //public int plunges;
+    //public int plungeGoal;
+    public GameObject toiletPrefab;
+    //public GameObject InvisibleWall;
+    public GameObject winImg;
+    public toiletOverlord[] toiletCount;
+    public int uncloggedCount;
+    public int uncloggedGoal;
+    public Vector3[] positionsEasy;
+    public Vector3[] positionsNormal;
+    public Vector3[] positionsHard;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MinigameInit();
+    }
+
+    void MinigameInit()
+    {
+        switch (skillStatic.Skill)
+        {
+            case 0:
+                foreach (Vector3 pos in positionsEasy)
+                {
+                    GameObject.Instantiate(toiletPrefab, pos, transform.rotation);
+                }
+                break;
+            case 1:
+                foreach (Vector3 pos in positionsNormal)
+                {
+                    GameObject.Instantiate(toiletPrefab, pos, transform.rotation);
+                }
+                break;
+            case 2:
+                foreach (Vector3 pos in positionsHard)
+                {
+                    GameObject.Instantiate(toiletPrefab, pos, transform.rotation);
+                }
+                break;
+        }
+        toiletCount = FindObjectsByType<toiletOverlord>(FindObjectsSortMode.None);
+        uncloggedGoal = toiletCount.Length;
+        //playerPlunge = FindFirstObjectByType<MouseMovement>();
+        //plungeGoal = (Random.Range(4, 8));   
+    }
+
+
+    public void winCondition()
+    {
+        winImg.SetActive(true);
+    }
+    public void SetSkill(int skillet)
+    {
+        skillStatic.Skill = skillet;
+        SceneManager.LoadScene("Skibidi");
+    }
+}
