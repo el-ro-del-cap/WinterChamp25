008bcf5e1c4da4642b48e9e01ddf35bc70e72de7
Zera <guillegar17@gmail.com>
2025-07-26 12:30:11 -0300

    Juego de estratamagamas hecho

---
+++ Assets/StratagemGame/StratagemGame.cs
@@ -0,0 +1,265 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StratagemGame : MonoBehaviour {
+
+    string lastCode = "";
+
+    public string[] defaultCodes = new string[] { 
+        "wdsss",
+        "wsdaw",
+        "sswd",
+        "swaswdsw",
+        "dwws",
+        "asdawd",
+        "dsawas",
+        "sss",
+        "wswsws",
+        "adadad",
+        "dddaaa",
+        "wdsd",
+        "sadda",
+        "ddsd",
+        "dsaww",
+        "dswwass",
+        "sdddwasdaw",
+        "wdsaw",
+        "adsas",
+        "wwddssaa",
+        "dwasdwas",
+        "wasd",
+        "wwwwwwwwws",
+        "asdwdsdw",
+        "asdsdsw",
+        "asdwasw",
+        "dwasdaw",
+        "a",
+        "sdaswdawdaswadswads"
+    };
+
+    public int victoryCount;
+
+    public StratagemArrow UPArrowPrefab;
+    public StratagemArrow LEFTArrowPrefab;
+    public StratagemArrow DOWNArrowPrefab;
+    public StratagemArrow RIGHTArrowPrefab;
+
+    public Transform layoutForArrows;
+
+    public KeyCode[] upArrowAliases = new KeyCode[] {KeyCode.W, KeyCode.UpArrow};
+    public KeyCode[] leftArrowAliases = new KeyCode[] { KeyCode.A, KeyCode.LeftArrow };
+    public KeyCode[] downArrowAliases = new KeyCode[] { KeyCode.S, KeyCode.DownArrow };
+    public KeyCode[] rightArrowAliases = new KeyCode[] { KeyCode.D, KeyCode.RightArrow };
+
+    public bool playing;
+
+    /// <summary>
+    /// Settear en false luego de usarlo
+    /// </summary>
+    public bool gameVictory = false;
+
+    List<StratArrowContainer> newArrows;
+    List<StratArrowContainer> usedArrows;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        newArrows = new List<StratArrowContainer>();
+        usedArrows = new List<StratArrowContainer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (playing) {
+            if (newArrows.Count == 0) {
+                GameVictory();
+            }
+            if (Input.anyKeyDown) {
+                CheckKeyPresses();
+            }
+
+        }
+    }
+
+    private void CheckKeyPresses() {
+
+        foreach (KeyCode k in upArrowAliases) {
+            if (Input.GetKeyDown(k)) {
+                PressKey(ArrowDirection.up);
+                return;
+            }
+        }
+        foreach (KeyCode k in leftArrowAliases) {
+            if (Input.GetKeyDown(k)) {
+                PressKey(ArrowDirection.left);
+                return;
+            }
+        }
+        foreach (KeyCode k in downArrowAliases) {
+            if (Input.GetKeyDown(k)) {
+                PressKey(ArrowDirection.down);
+                return;
+            }
+        }
+        foreach (KeyCode k in rightArrowAliases) {
+            if (Input.GetKeyDown(k)) {
+                PressKey(ArrowDirection.right);
+                return;
+            }
+        }
+
+    }
+
+
+    [ContextMenu("Run Test")]
+    public void LoadTest() {
+        StartArrowGame(defaultCodes[0]);
+        
+    }
+
+    private void PressKey(ArrowDirection direction) {
+        if (newArrows.Count == 0) {
+            GameVictory();
+        }
+        if (newArrows[0].direction == direction) {
+            newArrows[0].arrowScript.PressArrow();
+            usedArrows.Add(newArrows[0]);
+            newArrows.RemoveAt(0);
+        } else {
+            RestoreArrows();
+        }
+    }
+
+    public void GameVictory() {
+        StopGame();
+        gameVictory = true;
+
+    }
+
+    public void RestoreArrows() {
+        List<StratArrowContainer> auxList = usedArrows;
+        for (int i = 0; i < newArrows.Count; i++) {
+            auxList.Add(newArrows[i]);
+        }
+        foreach (StratArrowContainer arrow in auxList) {
+            arrow.arrowScript.UnPressArrow();
+        }
+        newArrows = auxList;
+        usedArrows = new List<StratArrowContainer>();
+    }
+
+
+    /// <summary>
+    /// Comienza juego de flechas estilo estratagemas de Helldivers con el codigo deseado. El codigo está compuesto de las flechas arriba, izquierda, abajo, derecha, representadas por caracteres "wasd"
+    /// </summary>
+    /// <param name="arrowCode"></param>
+    /// <returns></returns>
+    public void StartArrowGame(string arrowCode) {
+        ClearArrows();
+        newArrows = new List<StratArrowContainer>();
+        usedArrows = new List<StratArrowContainer>();
+        for (int i = 0; i < arrowCode.Length; i++) {
+            switch (arrowCode[i]) {
+                case 'w':
+                    InstantiateArrow(UPArrowPrefab, ArrowDirection.up);
+                    break;
+                case 'a':
+                    InstantiateArrow(LEFTArrowPrefab, ArrowDirection.left);
+                    break;
+                case 's':
+                    InstantiateArrow(DOWNArrowPrefab, ArrowDirection.down);
+                    break;
+                case 'd':
+                    InstantiateArrow(RIGHTArrowPrefab, ArrowDirection.right);
+                    break;
+                default:
+                    break;
+            }
+        }
+        playing = true;
+
+
+    }
+
+    private void InstantiateArrow(StratagemArrow arrowToInstantiate, ArrowDirection direction) {
+        StratagemArrow thisArrow = GameObject.Instantiate(arrowToInstantiate, layoutForArrows);
+        StratArrowContainer arrowCont = new StratArrowContainer(thisArrow, direction);
+        newArrows.Add(arrowCont);
+    }
+
+    public void ClearArrows() {
+        foreach (StratArrowContainer arrow in newArrows) {
+            Destroy(arrow.arrowScript.gameObject);
+        }
+        newArrows.Clear();
+        foreach (StratArrowContainer arrow in usedArrows) {
+            Destroy(arrow.arrowScript.gameObject);
+        }
+        usedArrows.Clear();
+    }
+
+    public void StopGame() {
+        ClearArrows();
+        playing = false;
+    }
+
+    public void LoadRandomCode() {
+        RandomDefaultStart:
+        string code = defaultCodes[Random.Range(0, defaultCodes.Length)];
+        if (code == lastCode) {
+            goto RandomDefaultStart;
+        }
+        lastCode = code;
+        StartArrowGame(code);
+    }
+
+    private Coroutine frenzyCR;
+
+    public void StartCodeFrenzy() {
+        LoadRandomCode();
+        frenzyCR = StartCoroutine(CodeFrenzyCR());
+    }
+
+    public IEnumerator CodeFrenzyCR() {
+        while (true) {
+            if (gameVictory) {
+                gameVictory = false;
+                LoadRandomCode();
+            }
+            yield return null;
+        }
+    }
+
+    public void StopFrenzy() {
+        if (frenzyCR != null) {
+            StopCoroutine(frenzyCR);
+        }
+        StopGame();
+    }
+
+
+}
+
+public class StratArrowContainer {
+    public StratagemArrow arrowScript;
+    public ArrowDirection direction;
+
+    public StratArrowContainer() {
+        
+    }
+
+
+    public StratArrowContainer(StratagemArrow arrowScript, ArrowDirection arrowDirection) {
+        this.direction = arrowDirection;
+        this.arrowScript = arrowScript;
+    }
+}
+
+public enum ArrowDirection {
+    up,
+    left,
+    down,
+    right
+}
